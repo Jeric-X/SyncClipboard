fbb044675ae44fb8f025849eb3040c71abf3e3a0
Jeirc-X <ddjiangruochen@hotmail.com>
2021-03-05 18:02:37 +0800

    增加图片支持

--- SyncClipboard/Service/Profile/ImageProfile.cs
+++ SyncClipboard/Service/Profile/ImageProfile.cs
@@ -1,10 +1,13 @@
-﻿using static SyncClipboard.Service.ProfileType;
+﻿using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using SyncClipboard.Utility;
+using static SyncClipboard.Service.ProfileType;
 
 namespace SyncClipboard.Service
 {
     class ImageProfile : FileProfile
     {
-        private string fullpath = "";
         public ImageProfile(string filepath) : base(filepath)
         {
         }
@@ -18,12 +21,57 @@ namespace SyncClipboard.Service
             return ClipboardType.Image;
         }
 
-        // protected override void SetContentToLocalClipboard()
-        // {
-        //     base.SetContentToLocalClipboard();
-        //     // string imagePath = GetTempLocalFilePath();
-        //     // string html = $@"<img src=""file:///{imagePath}"">";
-        //     // ClipboardHelper.CopyToClipboard(html, "");
-        // }
+        private void SetBitmap(DataObject dataObject, string imagePath)
+        {
+            FileStream fileStream = new FileStream(imagePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            byte[] bytes = new byte[fileStream.Length];
+            fileStream.Read(bytes, 0, bytes.Length);
+            fileStream.Close();
+
+            Stream stream = new MemoryStream(bytes);
+
+            var bitmap = new Bitmap(stream);
+            stream.Close();
+
+            dataObject.SetData(DataFormats.Bitmap, bitmap);
+        }
+
+        private void SetHtml(DataObject dataObject, string imagePath)
+        {
+            string html = $@"<img src=""file:///{imagePath}"">";
+            string clipboardHtml = ClipboardHtmlBuilder.GetClipboardHtml(html);
+            dataObject.SetData(DataFormats.Html, clipboardHtml);
+        }
+
+        private static string clipboardQqFormat = @"<QQRichEditFormat>
+<Info version=""1001"">
+</Info>
+<EditElement type=""1"" imagebiztype=""0"" textsummary="""" filepath=""<<<<<<"" shortcut="""">
+</EditElement>
+</QQRichEditFormat>";
+
+        private void SetQqFormat(DataObject dataObject, string imagePath)
+        {
+            string clipboardQq = clipboardQqFormat.Replace("<<<<<<", imagePath);
+            MemoryStream ms = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(clipboardQq));
+            dataObject.SetData("QQ_Unicode_RichEdit_Format", ms);
+        }
+
+        protected override DataObject CreateDataObject()
+        {
+            var dataObject = base.CreateDataObject();
+            if (dataObject is null)
+            {
+                return null;
+            }
+
+            string imagePath = GetTempLocalFilePath();
+
+            SetHtml(dataObject, imagePath);
+            SetQqFormat(dataObject, imagePath);
+            SetBitmap(dataObject, imagePath);
+
+            return dataObject;
+        }
     }
 }
---
+++ SyncClipboard/Utility/ClipboardHtmlBuilder.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+namespace SyncClipboard.Utility
+{
+    public static class ClipboardHtmlBuilder
+    {
+        private const string Header = @"Version:0.9
+StartHTML:<<<<<<<<1
+EndHTML:<<<<<<<<2
+StartFragment:<<<<<<<<3
+EndFragment:<<<<<<<<4";
+
+        private const string StartFragment = @"<!--StartFragment-->";
+        private const string EndFragment = @"<!--EndFragment-->";
+
+        public static string GetClipboardHtml(string html)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(Header);
+
+            sb.Append("<html><body>");
+            sb.Append(StartFragment);
+            int fragmentStart = GetByteCount(sb);
+            sb.Append(html);
+            int fragmentEnd = GetByteCount(sb);
+            sb.Append(EndFragment);
+            sb.Append("</body></html>");
+
+            sb.Replace("<<<<<<<<1", Header.Length.ToString("D9"), 0, Header.Length);
+            sb.Replace("<<<<<<<<2", GetByteCount(sb).ToString("D9"), 0, Header.Length);
+            sb.Replace("<<<<<<<<3", fragmentStart.ToString("D9"), 0, Header.Length);
+            sb.Replace("<<<<<<<<4", fragmentEnd.ToString("D9"), 0, Header.Length);
+
+            return sb.ToString();
+        }
+
+        private static int GetByteCount(StringBuilder sb, int start = 0, int end = -1)
+        {
+            int count = 0;
+            end = end > -1 ? end : sb.Length;
+
+            string str = sb.ToString().Substring(start, end);
+            count = Encoding.UTF8.GetByteCount(str);
+            return count;
+        }
+    }
+}
