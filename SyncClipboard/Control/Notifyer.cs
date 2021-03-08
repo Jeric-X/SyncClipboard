9644f08c3cbf57af515841fc42da1cb758fcbbb2
Jeirc-X <ddjiangruochen@hotmail.com>
2021-03-08 16:00:54 +0800

    添加动态图标支持

--- SyncClipboard/Control/Notifyer.cs
+++ SyncClipboard/Control/Notifyer.cs
@@ -1,39 +1,45 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace SyncClipboard.Control
 {
     class Notifyer
     {
-        private NotifyIcon notifyIcon;
-        private string notifyText;
+        private NotifyIcon _notifyIcon;
+        private string _notifyText;
+
+        private System.Timers.Timer _iconTimer;
+        private Icon[] _dynamicIcons;
+        private static Icon _defaultIcon = Properties.Resources.upload;
+        private int _iconIndex = 1;
 
         public Notifyer(ContextMenu contextMenu)
         {
-            this.notifyIcon = new System.Windows.Forms.NotifyIcon();
-            this.notifyIcon.ContextMenu = contextMenu;
-            this.notifyIcon.Icon = Properties.Resources.upload;
-            this.notifyIcon.Text = "SyncClipboard";
-            this.notifyIcon.Visible = true;
-            this.notifyIcon.BalloonTipClicked += new System.EventHandler(this.notifyIcon_BalloonTipClicked);
+            this._notifyIcon = new System.Windows.Forms.NotifyIcon();
+            this._notifyIcon.ContextMenu = contextMenu;
+            this._notifyIcon.Icon = _defaultIcon;
+            this._notifyIcon.Text = "SyncClipboard";
+            this._notifyIcon.Visible = true;
+            this._notifyIcon.BalloonTipClicked += new System.EventHandler(this.notifyIcon_BalloonTipClicked);
         }
 
         public void SetDoubleClickEvent(EventHandler eventHandler)
         {
-            this.notifyIcon.DoubleClick += eventHandler;
+            this._notifyIcon.DoubleClick += eventHandler;
         }
 
         public void Exit()
         {
-            this.notifyIcon.Visible = false;
+            this._notifyIcon.Visible = false;
         }
 
         private void notifyIcon_BalloonTipClicked(object sender, EventArgs e)
         {
-            if (notifyText == null || notifyText.Length < 4)
+            if (_notifyText == null || _notifyText.Length < 4)
                 return;
-            if (notifyText.Substring(0, 4) == "http" || notifyText.Substring(0, 4) == "www.")
-                System.Diagnostics.Process.Start(this.notifyText);
+            if (_notifyText.Substring(0, 4) == "http" || _notifyText.Substring(0, 4) == "www.")
+                System.Diagnostics.Process.Start(this._notifyText);
         }
 
         public void setLog(bool notify, bool notifyIconText, string title, string content, string contentSimple, string level)
@@ -42,23 +48,23 @@ namespace SyncClipboard.Control
             {
                 if (notify)
                 {
-                    notifyText = content;
+                    _notifyText = content;
                     if (!string.IsNullOrEmpty(content))
                     {
-                        this.notifyIcon.ShowBalloonTip(5, title, SafeMessage(content), ToolTipIcon.None);
+                        this._notifyIcon.ShowBalloonTip(5, title, SafeMessage(content), ToolTipIcon.None);
                     }
                 }
                 if (notifyIconText)
                 {
-                    this.notifyIcon.Text = Program.SoftName + "\n" + title + "\n" + contentSimple;
+                    this._notifyIcon.Text = Program.SoftName + "\n" + title + "\n" + contentSimple;
                 }
                 if (level == "erro")
                 {
-                    notifyIcon.Icon = Properties.Resources.erro;
+                    _notifyIcon.Icon = Properties.Resources.erro;
                 }
                 else if (level == "info")
                 {
-                    notifyIcon.Icon = Properties.Resources.upload;
+                    _notifyIcon.Icon = Properties.Resources.upload;
                 }
             }
             catch (Exception)
@@ -79,6 +85,48 @@ namespace SyncClipboard.Control
             }
 
             return str;
+        }
+
+        public void SetDynamicNotifyIcon(Icon[] icons, int delayTime)
+        {
+            if (icons.Length <= 0)
+            {
+                return;
+            }
+
+            _dynamicIcons = icons;
+            _notifyIcon.Icon = _dynamicIcons[0];
+            _iconTimer = new System.Timers.Timer(delayTime);
+            _iconTimer.Elapsed += SetNextDynamicNotifyIcon;
+            _iconTimer.AutoReset = true;
+            _iconTimer.Start();
+        }
+
+        public void StopDynamicNotifyIcon()
+        {
+            _iconTimer.Stop();
+            _iconTimer.Close();
+            _iconTimer = null;
+
+            _dynamicIcons = null;
+            _iconIndex = 1;
+            _notifyIcon.Icon = _defaultIcon;
+        }
+
+        private void SetNextDynamicNotifyIcon(object sender, EventArgs e)
+        {
+            if (_dynamicIcons is null || _dynamicIcons.Length == 0)
+            {
+                return;
+            }
+
+            if (_iconIndex >= _dynamicIcons.Length)
+            {
+                _iconIndex = 0;
+            }
+
+            _notifyIcon.Icon = _dynamicIcons[_iconIndex];
+            _iconIndex++;
         }
     }
 }
