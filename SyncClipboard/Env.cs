c015daa87a596fe7ca30ca25110023c7b1831422
Jeirc-X <ddjiangruochen@hotmail.com>
2022-04-23 23:47:25 +0800

    v1.3.8

--- SyncClipboard/Env.cs
+++ SyncClipboard/Env.cs
@@ -2,7 +2,7 @@ namespace SyncClipboard
 {
     internal static class Env
     {
-        public const string VERSION = "1.3.7";
+        public const string VERSION = "1.3.8";
         private const string PATH_SLASH = @"\";
         internal static readonly string Directory = System.Windows.Forms.Application.StartupPath;
 
