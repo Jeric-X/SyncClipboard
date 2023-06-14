0e577362fc9b100caca94c72aab6e941f6d55c02
Jeirc-X <ddjiangruochen@hotmail.com>
2023-06-14 20:27:11 +0800

    导航列表收起合并

--- SyncClipboard.WinUI3/Views/SettingWindow.xaml.cs
+++ SyncClipboard.WinUI3/Views/SettingWindow.xaml.cs
@@ -2,7 +2,13 @@ using Microsoft.UI;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
 using System;
+using Windows.UI.WindowManagement;
+using CommunityToolkit.Mvvm.ComponentModel;
+//using static System.Net.Mime.MediaTypeNames;
+using CommunityToolkit.Mvvm.Input;
+using Application = Microsoft.UI.Xaml.Application;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -19,11 +25,14 @@ namespace SyncClipboard.WinUI3.Views
             this.InitializeComponent();
 
             var titleBar = AppWindow.TitleBar;
-            titleBar.ExtendsContentIntoTitleBar = true;
+            //titleBar.ExtendsContentIntoTitleBar = true;
+
+            ExtendsContentIntoTitleBar = true;
+            SetTitleBar(AppTitleBar);
             // 标题栏按键背景色设置为透明
-            titleBar.ButtonBackgroundColor = Colors.Transparent;
-            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
-            titleBar.ButtonForegroundColor = Colors.Black;
+            //titleBar.ButtonBackgroundColor = Colors.Transparent;
+            //titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+            //AppWindow.TitleBar.ButtonForegroundColor = Colors.Black;
             AppWindow.ResizeClient(new(1200, 700));
         }
 
@@ -37,17 +46,28 @@ namespace SyncClipboard.WinUI3.Views
 
         private void Window_SizeChanged(object sender, WindowSizeChangedEventArgs args)
         {
-            if (args.Size.Width < 600)
+            if (args.Size.Width < 800)
             {
-                MenuList.Visibility = Visibility.Collapsed;
-                Console.WriteLine(MenuList.Width);
-                //MainGrid.ColumnDefinitions[0].Width = new GridLength(0, GridUnitType.Pixel);
+                SplitPane.IsPaneOpen = false;
+                SplitPane.DisplayMode = SplitViewDisplayMode.Overlay;
+                ShowNavigeMenuButton.Visibility = Visibility.Visible;
+                SplitPane.PaneBackground = (SolidColorBrush)Application.Current.Resources["LayerOnMicaBaseAltFillColorTertiaryBrush"];
+                AppTitle.Margin = new(0, 0, 0, 0);
             }
             else
             {
-                //MainGrid.ColumnDefinitions[0].Width = new GridLength(200, GridUnitType.Pixel);
-                MenuList.Visibility = Visibility.Visible;
+                SplitPane.DisplayMode = SplitViewDisplayMode.Inline;
+                SplitPane.IsPaneOpen = true;
+                ShowNavigeMenuButton.Visibility = Visibility.Collapsed;
+                SplitPane.PaneBackground = (SolidColorBrush)Application.Current.Resources["LayerOnMicaBaseAltFillColorTransparentBrush"];
+                AppTitle.Margin = new(18, 0, 0, 0);
             }
+        }
+
+        [RelayCommand]
+        private void ShowHideWindow()
+        {
+            SplitPane.IsPaneOpen = !SplitPane.IsPaneOpen;
         }
     }
 }
