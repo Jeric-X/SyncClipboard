903174a28b83864cbf0b271d090ab46b3fb659f1
Jeirc-X <ddjiangruochen@hotmail.com>
2023-06-20 22:55:32 +0800

    Context Menu

--- SyncClipboard.WinUI3/Views/TrayIcon.xaml.cs
+++ SyncClipboard.WinUI3/Views/TrayIcon.xaml.cs
@@ -1,5 +1,8 @@
 using H.NotifyIcon;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml;
+using SyncClipboard.Core.Interface;
+using Windows.Foundation;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -8,6 +11,21 @@ namespace SyncClipboard.WinUI3.Views
 {
     public sealed partial class TrayIcon : TaskbarIcon
     {
+        // new MenuFlyoutSeparator()'s DesiredSize is (0.0, 0.0)
+        // MenuFlyoutItem's DesiredSize.Width is wrong before fist poping up
+        // Use this to tell the real Separator's height and MinWidth
+        public Size SeparatorSize
+        {
+            get
+            {
+                _Separator.Visibility = Visibility.Visible;
+                _Separator.Measure(new(10000.0, 10000.0));
+                var desiredSize = _Separator.DesiredSize;
+                _Separator.Visibility = Visibility.Collapsed;
+                return desiredSize;
+            }
+        }
+
         public TrayIcon()
         {
             this.InitializeComponent();
@@ -17,6 +35,15 @@ namespace SyncClipboard.WinUI3.Views
         {
             App.Current.ExitApp();
             this.Dispose();
+        }
+
+        private void MenuFlyoutItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            var menu = App.Current.Services.GetService<IContextMenu>();
+            menu?.AddMenuItemGroup(new[]
+            {
+                new MenuItem { Text = "added" }
+            });
         }
     }
 }
--- SyncClipboard.WinUI3/Views/TrayIconContextMenu.cs
+++ SyncClipboard.WinUI3/Views/TrayIconContextMenu.cs
@@ -1,23 +1,112 @@
-﻿using SyncClipboard.Core.Interface;
+﻿using CommunityToolkit.Mvvm.Input;
+using H.NotifyIcon;
+using H.NotifyIcon.Core;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using SyncClipboard.Core.Interface;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace SyncClipboard.WinUI3.Views
 {
     public class TrayIconContextMenu : IContextMenu
     {
         private readonly TrayIcon _trayIcon;
+        private ushort _index = default;
+
+        # region H.NotifyIcon.TaskbarIcon反射信息
+
+        private readonly PropertyInfo _pInfoContextMenuFlyout;
+        private readonly PropertyInfo _pInfoIsContextMenuVisible;
+        private readonly PropertyInfo _pInfoContextMenuWindowHandle;
+
+        private readonly MenuFlyout _menuFlyout;
+        private readonly nint _handle;
+
+        private void InsertItem(ushort index, MenuFlyoutItemBase flyoutItemBase)
+        {
+            PrepareItemForSecondWindow(flyoutItemBase);
+            _menuFlyout.Items.Insert(index, flyoutItemBase);
+        }
+
+        private void PrepareItemForSecondWindow(MenuFlyoutItemBase flyoutItemBase)
+        {
+            if (flyoutItemBase is not MenuFlyoutSeparator)
+            {
+                flyoutItemBase.Height = 32;
+                flyoutItemBase.Padding = new Thickness(11, 0, 11, 0);
+            }
+
+            flyoutItemBase.Tapped += (_, _) =>
+            {
+                _pInfoIsContextMenuVisible.SetValue(_trayIcon, false);
+                _menuFlyout.Hide();
+                _ = WindowUtilities.HideWindow(_handle);
+            };
+        }
+
+        #endregion
+
         public TrayIconContextMenu(SettingWindow mainWindow)
         {
             _trayIcon = mainWindow.TrayIcon;
+
+            #region 初始化H.NotifyIcon.TaskbarIcon反射信息
+            const BindingFlags flags = BindingFlags.NonPublic | BindingFlags.Instance;
+
+            if (typeof(TaskbarIcon).GetProperty("ContextMenuFlyout", flags) is PropertyInfo pInfoContextMenuFlyout &&
+                 typeof(TaskbarIcon).GetProperty("IsContextMenuVisible", flags) is PropertyInfo pInfoIsContextMenuVisible &&
+                 typeof(TaskbarIcon).GetProperty("ContextMenuWindowHandle", flags) is PropertyInfo pInfoContextMenuWindowHandle)
+            {
+                _pInfoContextMenuFlyout = pInfoContextMenuFlyout;
+                _pInfoIsContextMenuVisible = pInfoIsContextMenuVisible;
+                _pInfoContextMenuWindowHandle = pInfoContextMenuWindowHandle;
+            }
+            else
+            {
+                throw new Exception("Can not get PropertyInfo? of TrayIcon");
+            }
+
+            var handle = _pInfoContextMenuWindowHandle.GetValue(_trayIcon) as nint?;
+            if (_pInfoContextMenuFlyout.GetValue(_trayIcon, null) is MenuFlyout menuFlyout && handle is not null)
+            {
+                _menuFlyout = menuFlyout;
+                _handle = handle.Value;
+            }
+            else
+            {
+                throw new Exception("Can not get Property of TrayIcon");
+            }
+            # endregion
         }
 
         void IContextMenu.AddMenuItemGroup(MenuItem[] menuItems)
         {
-            throw new NotImplementedException();
+            foreach (var item in menuItems)
+            {
+                MenuFlyoutItem flyoutItem = new()
+                {
+                    Text = item.Text,
+                };
+
+                if (item.Action is not null)
+                {
+                    flyoutItem.Command = new RelayCommand(item.Action);
+                }
+
+                InsertItem(_index++, flyoutItem);
+            }
+
+            InsertItem(_index++, CreateSeparator());
+        }
+
+        private MenuFlyoutSeparator CreateSeparator()
+        {
+            return new MenuFlyoutSeparator
+            {
+                Height = _trayIcon.SeparatorSize.Height,
+                Width = _trayIcon.SeparatorSize.Width
+            };
         }
     }
 }
