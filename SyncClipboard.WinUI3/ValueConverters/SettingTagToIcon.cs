2b19081062198febab78a596478207a6b123a725
Jeirc-X <ddjiangruochen@hotmail.com>
2023-08-05 00:56:22 +0800

    UI: 设置列表图标

---
+++ SyncClipboard.WinUI3/ValueConverters/SettingTagToIcon.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace SyncClipboard.WinUI3.ValueConverters;
+
+public class SettingTagToIcon : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        string tag = (string)value;
+        return tag switch
+        {
+            "SyncSetting" => "\uEBD3",
+            "SystemSetting" => "\uE115",
+            _ => "\uE115",
+        };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
+
