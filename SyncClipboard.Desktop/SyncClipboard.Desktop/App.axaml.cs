2f1989372b2b102c9a09807d027c28645e305725
Jeirc-X <jericx@jericx.xyz>
2023-10-18 22:31:18 +0800

    TrayIconContextMenu

--- SyncClipboard.Desktop/SyncClipboard.Desktop/App.axaml.cs
+++ SyncClipboard.Desktop/SyncClipboard.Desktop/App.axaml.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
+using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.DependencyInjection;
 using SyncClipboard.Core;
 using SyncClipboard.Desktop.ViewModels;
@@ -17,6 +18,7 @@ public partial class App : Application
     public Window? MainWindow { get; private set; }
     private ProgramWorkflow? ProgramWorkflow;
     public new static App Current => (App)Application.Current!;
+    private IClassicDesktopStyleApplicationLifetime appLife;
 
     public App()
     {
@@ -41,6 +43,7 @@ public partial class App : Application
                 DataContext = new MainViewModel()
             };
             desktop.MainWindow = MainWindow;
+            appLife = desktop;
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
@@ -50,8 +53,8 @@ public partial class App : Application
             };
         }
 
-        ProgramWorkflow = new ProgramWorkflow(Services);
-        ProgramWorkflow.Run();
+        //ProgramWorkflow = new ProgramWorkflow(Services);
+        //ProgramWorkflow.Run();
         base.OnFrameworkInitializationCompleted();
     }
 }
---
+++ SyncClipboard.Desktop/SyncClipboard.Desktop/Views/TrayIconContextMenu.cs
@@ -0,0 +1,70 @@
+﻿using Avalonia.Controls;
+using CommunityToolkit.Mvvm.Input;
+using SyncClipboard.Core.AbstractClasses;
+using SyncClipboard.Core.Interfaces;
+using System;
+
+namespace SyncClipboard.Desktop.Views;
+
+internal class TrayIconContextMenu : ContextMenuBase
+{
+    private readonly NativeMenu _menu;
+    private readonly int _menuReserveCount;
+
+    public TrayIconContextMenu()
+    {
+        var icons = TrayIcon.GetIcons(App.Current);
+        var trayIcon = icons?[0];
+        var menu = trayIcon?.Menu;
+        ArgumentNullException.ThrowIfNull(menu, nameof(menu));
+        _menu = menu;
+        _menuReserveCount = _menu.Items.Count;
+    }
+
+    private void InsertItem(int index, NativeMenuItemBase menuItemBase)
+    {
+        _menu.Items.Insert(index, menuItemBase);
+    }
+
+    protected override void InsertMenuItem(int index, Core.Interfaces.MenuItem menuitem)
+    {
+        NativeMenuItem item = new()
+        {
+            Header = menuitem.Text,
+        };
+
+        if (menuitem.Action is not null)
+        {
+            item.Command = new RelayCommand(menuitem.Action);
+        }
+        InsertItem((ushort)index, item);
+    }
+
+    protected override void InsertSeparator(int index)
+    {
+        InsertItem(index, new NativeMenuItemSeparator());
+    }
+
+    protected override void InsertToggleMenuItem(int index, ToggleMenuItem menuitem)
+    {
+        NativeMenuItem item = new()
+        {
+            Header = menuitem.Text,
+            IsChecked = menuitem.Checked,
+            ToggleType = NativeMenuItemToggleType.CheckBox
+        };
+
+        menuitem.CheckedChanged += (bool status) => item.IsChecked = status;
+
+        if (menuitem.Action is not null)
+        {
+            item.Command = new RelayCommand(menuitem.Action);
+        }
+        InsertItem((ushort)index, item);
+    }
+
+    protected override int MenuItemsCount()
+    {
+        return _menu.Items.Count - _menuReserveCount;
+    }
+}
