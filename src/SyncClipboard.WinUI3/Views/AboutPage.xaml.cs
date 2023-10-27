023d6590932eba3990cd1863f45318ea4fd2bd59
Jeirc-X <jericx@jericx.xyz>
2023-10-28 02:17:11 +0800

    升级SettingsCard到正式版

--- src/SyncClipboard.WinUI3/Views/AboutPage.xaml.cs
+++ src/SyncClipboard.WinUI3/Views/AboutPage.xaml.cs
@@ -1,4 +1,4 @@
-using CommunityToolkit.Labs.WinUI;
+using CommunityToolkit.WinUI.Controls;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media.Animation;
