13f70ab815f77547cd3556871c064794afadb003
JericX <jericx@jericx.xyz>
2023-10-30 00:46:54 +0800

    使用NSApplication.Hide代替隐藏

--- src/SyncClipboard.Desktop.MacOS/Views/MainWindow.axaml.cs
+++ src/SyncClipboard.Desktop.MacOS/Views/MainWindow.axaml.cs
@@ -1,15 +1,9 @@
 ﻿using Avalonia.Controls;
-using Avalonia.Media;
-using Avalonia.Threading;
-using FluentAvalonia.UI.Media.Animation;
-using SyncClipboard.Core.Interfaces;
-using SyncClipboard.Core.ViewModels;
 using System;
 using SyncClipboard.Desktop;
 using AppKit;
-using System.Reflection;
 using Avalonia;
-using ObjCRuntime;
+using System.Threading.Tasks;
 
 namespace SyncClipboard.Desktop.MacOS.Views;
 
@@ -17,14 +11,52 @@ public class MainWindow : SyncClipboard.Desktop.Views.MainWindow
 {
     protected override void OnClosing(WindowClosingEventArgs e)
     {
-        var window = NSApplication.SharedApplication.MainWindow;
-        window.Miniaturize(window);
+        NSApplication.SharedApplication.Hide(NSApplication.SharedApplication);
         e.Cancel = true;
     }
 
-    // protected override void ShowMainWindow()
-    // {
-    //     var window = NSApplication.SharedApplication.MainWindow;
-    //     window.Deminiaturize(window);
-    // }
+    public async void Init(bool hide)
+    {
+        if (hide is false)
+        {
+            base.ShowMainWindow();
+            return;
+        }
+
+        _created = true;
+        var transparencyLevelHint = TransparencyLevelHint;
+        var background = Background;
+        var extendClientAreaTitleBarHeightHint = ExtendClientAreaTitleBarHeightHint;
+        var opacity = Opacity;
+        var extendClientAreaChromeHints = ExtendClientAreaChromeHints;
+
+        TransparencyLevelHint = new WindowTransparencyLevel[] { WindowTransparencyLevel.Transparent };
+        Background = null;
+        ExtendClientAreaChromeHints = Avalonia.Platform.ExtendClientAreaChromeHints.NoChrome;
+        ExtendClientAreaTitleBarHeightHint = 0;
+        this.Opacity = 0;
+        ShowMainWindow();
+        NSApplication.SharedApplication.Hide(NSApplication.SharedApplication);
+
+        await Task.Delay(500).ConfigureAwait(true);
+        TransparencyLevelHint = transparencyLevelHint;
+        Background = background;
+        ExtendClientAreaTitleBarHeightHint = extendClientAreaTitleBarHeightHint;
+        this.Opacity = opacity;
+        ExtendClientAreaChromeHints = extendClientAreaChromeHints;
+    }
+
+    private bool _created = false;
+    protected override void ShowMainWindow()
+    {
+        if (_created)
+        {
+            NSApplication.SharedApplication.Unhide(NSApplication.SharedApplication);
+        }
+        else
+        {
+            base.ShowMainWindow();
+            _created = true;
+        }
+    }
 }
