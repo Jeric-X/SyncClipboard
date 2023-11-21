036c3d9287acff5314eb74b088dbf3daa3b41ee6
JericX <jericx@jericx.xyz>
2023-11-21 21:15:30 +0800

    提示信息

--- src/SyncClipboard.Core/UserServices/ClipboardService/EasyCopyImageSerivce.cs
+++ src/SyncClipboard.Core/UserServices/ClipboardService/EasyCopyImageSerivce.cs
@@ -99,11 +99,11 @@ public class EasyCopyImageSerivce : ClipboardHander
 
         if (DownloadWebImageEnabled && !string.IsNullOrEmpty(metaInfo.Html))
         {
-            TrayIcon.SetStatusString(SERVICE_NAME, "Downloading web image.");
             const string Expression = @".*<[\s]*img[\s]*.*?[\s]*src=(?<quote>[""'])(?<imgUrl>https?://.*?)\k<quote>.*?/[\s]*>";
             var match = Regex.Match(metaInfo.Html, Expression, RegexOptions.Compiled);    // 性能未测试，benchmark参考 https://www.bilibili.com/video/av441496306/?p=1&plat_id=313&t=15m53s
             if (match.Success) // 是从浏览器复制的图片
             {
+                TrayIcon.SetStatusString(SERVICE_NAME, "Downloading web image.");
                 _logger.Write(LOG_TAG, "http image url: " + match.Groups["imgUrl"].Value);
                 var uri = new Uri(match.Groups["imgUrl"].Value);
                 var localPath = await DownloadImage(uri, useProxy, cancellationToken);
