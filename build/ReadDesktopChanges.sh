0f860753e260af5024c42515d3a9fbd6d222e628
Jeirc-X <jericx@jericx.xyz>
2025-06-15 15:03:52 +0800

    删除过时文件

--- build/ReadDesktopChanges.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CHANGES=../src/SyncClipboard.Desktop/Changes.md
-
-cat $CHANGES | head -n 1 | tr -d "\n" > version.txt
-awk '{if ($0 == "") exit; else print}' $CHANGES > featuretemp.txt
-tail -n +2 featuretemp.txt > feature.txt
